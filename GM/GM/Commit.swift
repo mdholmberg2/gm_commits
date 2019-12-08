beece1043ce72bf4cecd19d680c36cef9bceef78
donHolmberg <mholmberg@austin.rr.com>
2019-12-08 14:58:54 -0600

    adds commit model

---
+++ GM/GM/Commit.swift
@@ -0,0 +1,15 @@
+//
+//  Commit.swift
+//  GM
+//
+//  Created by M D Holmberg II on 12/8/19.
+//  Copyright © 2019 M D Holmberg II. All rights reserved.
+//
+
+import Foundation
+
+struct Commit: Codable {
+    var author: String
+    var hash: String
+    var message: String
+}
