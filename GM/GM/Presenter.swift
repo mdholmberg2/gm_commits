57303a2d8adaf9e54d0288509c381db822d18498
donHolmberg <mholmberg@austin.rr.com>
2019-12-08 14:44:23 -0600

    adds interactor and presenter classes

---
+++ GM/GM/Presenter.swift
@@ -0,0 +1,13 @@
+//
+//  Presenter.swift
+//  GM
+//
+//  Created by M D Holmberg II on 12/8/19.
+//  Copyright © 2019 M D Holmberg II. All rights reserved.
+//
+
+import Foundation
+
+class Presenter {
+    
+}
