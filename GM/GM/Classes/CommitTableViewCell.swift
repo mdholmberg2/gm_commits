2bb3d17257ccf0ed94770f70556b4bd6b7be734f
donHolmberg <mholmberg@austin.rr.com>
2019-12-08 15:06:06 -0600

    adds commit cell and xib

---
+++ GM/GM/Classes/CommitTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CommitTableViewCell.swift
+//  GM
+//
+//  Created by M D Holmberg II on 12/8/19.
+//  Copyright © 2019 M D Holmberg II. All rights reserved.
+//
+
+import UIKit
+
+class CommitTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
